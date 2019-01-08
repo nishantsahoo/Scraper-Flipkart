5f6ffd5fb874a1b0ae9ae551e58a1b0e094216de
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-08 22:45:45 +0530

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,8 +1,16 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-queryTerm = ""
-queryTerm = input("Enter the query term: ")
-html = urlopen("https://www.flipkart.com/search?q=" + queryTerm)
-bsObj = BeautifulSoup(html.read());
-print(bsObj.title)
+def main():
+	queryTerm = ""
+	queryTerm = input("Enter the query term: ")
+	html = urlopen("https://www.flipkart.com/search?q=" + queryTerm)
+	soup = BeautifulSoup(html.read(), features="html.parser");
+	list_product = soup.findAll('div', attrs={'class': '_3wU53n'})
+	if list_product:
+		for product in list_product:
+			print(product.text)
+	else:
+		print("No results found!")
+
+main() # call of the main function
