234f09a08083c527cabd9d38c0d5f01776a08fc8
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-02-02 19:48:27 +0530

    List items

--- amazon-scraper.py
+++ amazon-scraper.py
@@ -5,11 +5,11 @@ def main():
 	queryTerm = ""
 	queryTerm = input("Enter the query term: ")
 	html = urlopen("https://www.amazon.in/s/field-keywords=" + queryTerm)
-	soup = BeautifulSoup(html.read(), features="html.parser");
-	list_product = soup.findAll('h2')[1:]
+	soup = BeautifulSoup(html.read(), features="html.parser")
+	list_product = soup.findAll('li', attrs={'class': 's-result-item celwidget'})[1:]
 	if list_product:
 		for product in list_product:
-			print(product.text)
+			print(product)
 	else:
 		print("No results found!")
 
