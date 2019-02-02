15be15b4de3f9cc77a19cb4f2ad75cb90902781f
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-02-02 20:18:02 +0530

    Update

--- amazon-scraper.py
+++ amazon-scraper.py
@@ -1,15 +1,27 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pprint
 
 def main():
 	queryTerm = ""
 	queryTerm = input("Enter the query term: ")
 	html = urlopen("https://www.amazon.in/s/field-keywords=" + queryTerm)
 	soup = BeautifulSoup(html.read(), features="html.parser")
-	list_product = soup.findAll('li', attrs={'class': 's-result-item celwidget'})[1:]
+	list_product = soup.findAll('div', attrs={'class': 'a-fixed-left-grid-col a-col-right'})
+	products = []
 	if list_product:
 		for product in list_product:
-			print(product)
+			if product.find('a', attrs={'class': 'a-link-normal a-text-normal'}):
+				name  = product.find('a', attrs={'class': 'a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal'}).text
+				price = product.find('a', attrs={'class': 'a-link-normal a-text-normal'}).text
+				product = {
+					'name': name,
+					'price': price
+				}
+				products += [product]
+
+		print(products)
+
 	else:
 		print("No results found!")
 
